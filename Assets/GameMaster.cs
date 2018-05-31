7fb1bd6e1bb5dd4d3e6a3eec5b2053ab00480bed
Alexey Glukhov <kanris2@gmail.com>
2018-05-31 10:27:20 +0300

    Add player spawn

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
-	public static void KillObject(GameObject objectToKill)
+    public static GameMaster gm;
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    private void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+        }
+    }
+
+    public IEnumerator RespawnPlayer()
+    {
+        Debug.Log("TODO: Player respawn audio");
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    public static void KillObject(GameObject objectToKill)
     {
         //Debug.Log(objectToKill.name);
         Destroy(objectToKill);
+        gm.StartCoroutine(gm.RespawnPlayer());
     }
+
+
 
 }
