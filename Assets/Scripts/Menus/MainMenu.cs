25e2e7929d35a6024a8e5c36d8391528a445f36f
Oleksii Hlukhov <kanris2@gmail.com>
2018-06-07 14:15:54 +0300

    Add Main Menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject loadingScene;
+    public Slider slider;
+
+    public string scene = "Main";
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadSceneAsync(scene));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator LoadSceneAsync(string scene)
+    {
+        var operation = SceneManager.LoadSceneAsync(scene);
+
+        loadingScene.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            var progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+	
+}
