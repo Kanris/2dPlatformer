a78f9e8d2268ec110c6d115112897b1fe1c7185e
Oleksii Hlukhov <kanris2@gmail.com>
2018-06-11 10:55:12 +0300

    Add moving platform

--- Assets/Scripts/Environment/MovingPlatform.cs
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -4,13 +4,56 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float m_offsetY = 20f;
+    public float m_moveTime = 2f;
+    Rigidbody2D body;
+    bool isMoving = false;
+    Vector3 nextPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = transform.GetComponent<Rigidbody2D>();
+
+        StartCoroutine(IdleAnimation(m_offsetY));
+    }
 
     private void FixedUpdate()
     {
+        if (isMoving)
+        {
+            Move();
+        }
+    }
+
+    IEnumerator IdleAnimation(float offsetY)
+    {
+
+        nextPoint = new Vector3(body.transform.position.x, body.transform.position.y - offsetY);
+        this.isMoving = true;
+
+        m_offsetY = offsetY;
+        yield return new WaitForSeconds(m_moveTime);
+        this.isMoving = false;
+
+        yield return IdleAnimation(-offsetY);
+    }
+
+    void Move()
+    {
+
+        Vector3 direction = (nextPoint - transform.position).normalized;
+        //direction *= Time.fixedDeltaTime;
+
+        //move ai
+        //body.AddForce(direction * 40000 * Time.fixedDeltaTime);
+        body.velocity = new Vector2(0, m_offsetY);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         
+        body.velocity = Vector2.zero;
+        //body.angularVelocity = Vector2.zero;
     }
 }
