182bd0958445bdbca1acd27f96bcdd6e0eb99450
Oleksii Hlukhov <kanris2@gmail.com>
2018-06-25 09:34:49 +0300

    Fix bug when MainMenu couldn't find LoadScene

--- Assets/Scripts/Weapon/MoveTrail.cs
+++ Assets/Scripts/Weapon/MoveTrail.cs
@@ -10,12 +10,15 @@ public class MoveTrail : MonoBehaviour {
 
 	// Update is called once per frame
     void Update () {
-        Debug.Log(LayerTohit.value);
         transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
         Destroy(gameObject, 1);
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if ( collision.gameObject.layer == 11)
+        {
+            Debug.LogError("Hit what i need");
+        }
     }
 }
